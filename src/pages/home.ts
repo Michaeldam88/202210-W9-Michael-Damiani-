57dd3f4e1dfbafc709a8c46be60a0911bf43faa7
Michaeldam88 <michaeldam88@gmail.com>
2022-12-05 13:42:47 +0100

    added the previuous and next event

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -4,29 +4,37 @@ import { PageCardResponseType } from '../models/pageCardsResponse.js';
 
 export class HomePage extends Component {
     url = 'https://pokeapi.co/api/v2/pokemon';
+    response!: PageCardResponseType;
 
     constructor(private selector: string) {
         super();
-        this.template = this.createTemplate();
-        this.render();
         this.init(this.url);
     }
 
-    async init(url:string) {
-        const response: PageCardResponseType = await (
-            await fetch(url)
-        ).json();
+    async init(url: string) {
+        this.response = await (await fetch(url)).json();
+        this.template = this.createTemplate();
+        this.render();
 
         try {
-            new List('[name=home]', response.results);
+            new List('[name=home]', this.response.results);
         } catch (error) {
             console.error(error);
         }
     }
 
     render() {
-        return super.innRender(this.selector);
-
+        const element = super.innRender(this.selector);
+        element.querySelector('#previous')?.addEventListener('click', () => {
+            if (!this.response.previous) return;
+            this.cleanHtml(this.selector);
+            this.init(this.response.previous);
+        });
+        element.querySelector('#next')?.addEventListener('click', () => {
+            this.cleanHtml(this.selector);
+            this.init(this.response.next);
+        });
+        return element;
     }
 
     private createTemplate() {
@@ -34,11 +42,11 @@ export class HomePage extends Component {
         <main>
             <div class="home-header">
             <h2>Pokemon List</h2>
-            <h3>Vistos 20/10000</h3>
+            <h3>Vistos 20 / ${this.response.count}</h3>
             </div>
             <slot name="home"></slot>
             <div class="change-page">
-            <button id="previuos" ><</button>
+            <button id="previous" ><</button>
             <button id="next">></button>
             </div>
         </main>
