327173c71cb4d5de0945f470fecd430bb9d99566
Michaeldam88 <michaeldam88@gmail.com>
2022-12-05 17:13:35 +0100

    added the first part of the modal

--- src/components/pokemonDetailMod.ts
+++ src/components/pokemonDetailMod.ts
@@ -1,9 +1,47 @@
-import { PokemonDetailType } from "../models/pokemonDetail.js";
-import { Component } from "./component.js";
+import { PokemonDetailType } from '../models/pokemonDetail.js';
+import { Component } from './component.js';
 
 export class PokemonDetailMod extends Component {
     constructor(private item: PokemonDetailType) {
         super();
-        
+        this.template = this.createTemplate();
+        this.render();
+    }
+
+    render() {
+        const element = super.innRender('.root');
+
+        return element;
+    }
+
+    private createTemplate() {
+        return `
+                <li class="item-card" id="id_${this.item.id}">
+                    <div>
+                    <img src="${
+                        this.item.sprites.other['official-artwork']
+                            .front_default
+                    }" alt="${this.item.name}">
+                    <p>nº ${this.item.id}</p>
+                    <h4>${
+                        this.item.name[0].toUpperCase() +
+                        this.item.name.substring(1)
+                    }</h4>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>Height: ${(this.item.height * 0.1).toFixed(
+                                1
+                            )} m</li>
+                            <li>Weight: ${this.item.weight} Kg</li>
+                            <li>Type: ${this.item.types[0].type.name}</li>
+                            <li> </li>
+                            <li> </li>
+                            <li> </li>
+                        </ul>
+                    </div>
+                    <button class="addFavorites"></button>
+                </li>
+        `;
     }
 }
